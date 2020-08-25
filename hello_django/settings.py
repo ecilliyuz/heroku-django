a34f971cc9c25c7f89e95c88341dc48a22a2ddbb
ecilliyuz <37616237+ecilliyuz@users.noreply.github.com>
2020-08-25 15:54:01 +0300

    Update settings.py

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", default="foo")
 
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "desolate-shore-09252.herokuapp.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "lit-plains-91059.herokuapp.com"]
 
 
 # Application definition
